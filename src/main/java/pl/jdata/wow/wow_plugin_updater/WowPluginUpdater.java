73602f90371af3d278c5677f9b7fbb1f21735f76
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2018-08-04 17:17:26 +0200

    Basic functionality.

--- src/main/java/pl/jdata/wow/wow_plugin_updater/WowPluginUpdater.java
+++ src/main/java/pl/jdata/wow/wow_plugin_updater/WowPluginUpdater.java
@@ -1,4 +1,28 @@
 package pl.jdata.wow.wow_plugin_updater;
 
+import java.io.IOException;
+
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
 public class WowPluginUpdater {
+
+    public static void main(String[] args) throws IOException {
+        final Terminal terminal = TerminalBuilder.builder()
+                .system(true)
+                .build();
+
+        final LineReader reader = LineReaderBuilder.builder()
+                .terminal(terminal)
+                .build();
+
+        while (true) {
+            String line = reader.readLine();
+
+        }
+    }
+
 }
+
